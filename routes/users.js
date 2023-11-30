df0f2bb147d5dd882287151a88629dc37920bbf2
SkyKai1018 <rgnkud@gmail.com>
2023-11-30 09:53:31 +0800

    修正： 1. 修正編輯及新增畫面於手機觀看時的排版 修改： 1. 暫時取消註冊功能避免超過免費容量限制

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,10 @@ const { storeReturnTo } = require('../middleware');
 const user = require('../models/user');
 const users = require('../controllers/users');
 
-router.route('/register')
-    .get(users.renderRegisterForm)
-    .post(cathAsync(users.registerUser));
+//暫時取消註冊功能
+// router.route('/register')
+//     .get(users.renderRegisterForm)
+//     .post(cathAsync(users.registerUser));
 
 router.route('/login')
     .get(users.renderLoginForm)
